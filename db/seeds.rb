5c511c29a10376b8ae7f93b5f4f70f9473afa8f4
Komenpurity <jepkorirpurity8@gmail.com>
2023-06-02 11:39:31 +0300

    completed

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Actor.create(first_name: "Tyrion", last_name: "Lanister")
+Show.create(name: "Kardashians",day: "31st May",season: "3",genre: "Reality")
+Character.create(name: "Khole", actor_id: 1, show_id: 1) 
+Network.create(call_letters: "Khole", channel: 124) 
